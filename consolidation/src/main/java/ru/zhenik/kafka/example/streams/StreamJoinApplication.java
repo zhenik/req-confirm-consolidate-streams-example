488da8d90908b6c354a10c7e1a3cd890cbbb81fa
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2019-02-22 11:17:18 +0100

    update processing

--- consolidation/src/main/java/ru/zhenik/kafka/example/streams/StreamJoinApplication.java
+++ consolidation/src/main/java/ru/zhenik/kafka/example/streams/StreamJoinApplication.java
@@ -19,7 +19,18 @@ import ru.zhenik.kafka.example.utils.Util;
 
 import static ru.zhenik.kafka.example.utils.Util.*;
 
-
+/**
+ * From doc about JoinWindow
+ *    stream1.ts - before <= stream2.ts <= stream1.ts + after
+ *
+ * Options:
+ * 1. before = after = time-difference
+ * 2. before = 0                AND after = time-difference
+ * 3. before = time-difference  AND after = 0
+ *
+ * When u do JoinWindows.of(Duration.ofSeconds(5)), is equal to
+ * stream1.ts - 5 sec <= stream2.ts <= stream1.ts + 5 sec
+ */
 public class StreamJoinApplication implements Runnable {
   private final KafkaStreams kafkaStreams;
   private final Util utils;
@@ -38,13 +49,14 @@ public class StreamJoinApplication implements Runnable {
     final KStream<String, String> confirmationsStream =
         streamsBuilder.stream(TOPIC_CONFIRMATION, Consumed.with(Serdes.String(), Serdes.String()));
 
-    final KStream<String, String> consolidatedStream = requestsStream.join(
+    final KStream<String, String> consolidatedStream = confirmationsStream.join(
         // other topic to join
-        confirmationsStream,
+        requestsStream,
+        //confirmationsStream,
         // left value, right value -> return value
-        (requestValue, confirmedValue) -> REQUEST_CONSOLIDATED,
+        (confirmedValue, requestValue) -> REQUEST_CONSOLIDATED,
         // window
-        JoinWindows.of(Duration.ofSeconds(5)),
+        JoinWindows.of(Duration.ofSeconds(0)).before(Duration.ofSeconds(5)),
         // how to join (ser and desers)
         Joined.with(
             Serdes.String(), /* key */
@@ -53,32 +65,93 @@ public class StreamJoinApplication implements Runnable {
         )
     );
 
-    //todo: consolidation-failed stream
+    final KStream<String, String> confirmationCameNeverStream = confirmationsStream.outerJoin(
+        // other topic to join
+        requestsStream,
+        //confirmationsStream,
+        // left value, right value -> return value
+        (confirmedValue, requestValue) ->
+            String.format("confirmationValue: %s ,requestValue: %s", confirmedValue, requestValue),
+        // window
+        JoinWindows.of(Duration.ofSeconds(0)).before(Duration.ofSeconds(5)),
+        // how to join (ser and desers)
+        Joined.with(
+            Serdes.String(), /* key */
+            Serdes.String(), /* left value */
+            Serdes.String()  /* right value */
+        )
+    );
 
-    final KStream<String, String> statusStream =
-        requestsStream
-            .outerJoin(
-                // other topic to join
-                consolidatedStream,
-                // left value, right value -> return value
-                (requestValue, confirmedValue) ->
-                    (confirmedValue == null) ? STATUS_NOT_CONSOLIDATED_YET : STATUS_CONSOLIDATED,
-                // window
-                JoinWindows.of(Duration.ofSeconds(5)),
-                // how to join (ser and desers)
-                Joined.with(
-                    Serdes.String(), /* key */
-                    Serdes.String(), /* left value */
-                    Serdes.String() /* right value */)
-            );
+    final KStream<String, String> confirmationCameLateStream = confirmationsStream.leftJoin(
+        // other topic to join
+        consolidatedStream,
+        //confirmationsStream,
+        // left value, right value -> return value
+        (confirmationValue, consolidatedValue) ->
+            String.format("confirmationValue: %s ,consolidatedValue: %s", confirmationValue, consolidatedValue),
+        // window
+        JoinWindows.of(Duration.ofSeconds(0)).after(Duration.ofSeconds(5)),
+        // how to join (ser and desers)
+        Joined.with(
+            Serdes.String(), /* key */
+            Serdes.String(), /* left value */
+            Serdes.String()  /* right value */
+        )
+    );
+
+    confirmationCameNeverStream.to("nt-confirmation-came-never");
+    confirmationCameLateStream.to("nt-confirmation-came-late");
 
     consolidatedStream
         .peek((k, v) -> System.out.println("Consolidated : "+k+" : "+v))
         .to(TOPIC_CONSOLIDATION, Produced.with(Serdes.String(), Serdes.String()));
 
-    statusStream
-        .peek((k, v) -> System.out.println("Status : " +k+" : "+v))
-        .to(TOPIC_STATUS, Produced.with(Serdes.String(), Serdes.String()));
+
+    //final KStream<String, String> failureRequestStream =
+    //    requestsStream.leftJoin(
+    //        consolidatedStream,
+    //        (requestValue, consolidatedValue) -> {
+    //          System.out.printf("Before LeftJoin: [%s : %s]\n", requestValue, consolidatedValue);
+    //          return String.format("requestValue: %s ,consolidatedValue: %s", requestValue, consolidatedValue);
+    //        },
+    //        // todo:  try with grace()
+    //        // todo: try with session windows
+    //        // https://kafka.apache.org/20/documentation/streams/developer-guide/dsl-api.html#session-windows
+    //        JoinWindows.of(Duration.ofSeconds(5)).after(Duration.ofSeconds(5)),
+    //        // how to join (ser and desers)
+    //        Joined.with(
+    //            Serdes.String(), /* key */
+    //            Serdes.String(), /* left value */
+    //            Serdes.String() /* right value */))
+    //    .peek((k,v)-> System.out.printf("After LeftJoin: [%s : %s]\n", k, v));
+    //
+    //failureRequestStream
+    //    .to(TOPIC_REQUEST_ERROR, Produced.with(Serdes.String(), Serdes.String()));
+
+
+    //todo: consolidation-failed stream
+
+    //final KStream<String, String> statusStream =
+    //    requestsStream
+    //        .outerJoin(
+    //            // other topic to join
+    //            consolidatedStream,
+    //            // left value, right value -> return value
+    //            (requestValue, confirmedValue) ->
+    //                (confirmedValue == null) ? STATUS_NOT_CONSOLIDATED_YET : STATUS_CONSOLIDATED,
+    //            // window
+    //            JoinWindows.of(Duration.ofSeconds(5)),
+    //            // how to join (ser and desers)
+    //            Joined.with(
+    //                Serdes.String(), /* key */
+    //                Serdes.String(), /* left value */
+    //                Serdes.String() /* right value */)
+    //        );
+
+
+    //statusStream
+    //    .peek((k, v) -> System.out.println("Status : " +k+" : "+v))
+    //    .to(TOPIC_STATUS, Produced.with(Serdes.String(), Serdes.String()));
 
     final Topology topology = streamsBuilder.build();
     System.out.println("Topology\n"+topology.describe());
